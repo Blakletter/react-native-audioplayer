b4d7a43703c705ec5cebaa6d246d605be214eaa8
Blakletter <48567475+Blakletter@users.noreply.github.com>
2019-03-19 09:47:02 -0700

    Method does not have @Override

--- android/src/main/java/com/sh3rawi/RNAudioPlayer/RNAudioPlayer.java
+++ android/src/main/java/com/sh3rawi/RNAudioPlayer/RNAudioPlayer.java
@@ -19,7 +19,7 @@ public class RNAudioPlayer implements ReactPackage {
     return modules;
   }
 
-  @Override
+
   public List<Class<? extends JavaScriptModule>> createJSModules() {
     return Collections.emptyList();
   }
